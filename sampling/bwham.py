f6d0f201c21fb75a56d7581c8a2934fe6709da94
jeongmoon2006 <78450740+jeongmoon2006@users.noreply.github.com>
2021-09-02 18:59:45 -0400

    Update bwham.py

--- sampling/bwham.py
+++ sampling/bwham.py
@@ -7,6 +7,8 @@ from matplotlib.pyplot import figure
 import autograd.numpy as nup
 from autograd import value_and_grad
 from scipy.optimize import minimize
+from autograd.extend import primitive
+from scipy.special import logsumexp
 
 
 class binwham:
@@ -66,45 +68,49 @@ class binwham:
         for i in range(self.S):
 
             ri = self.op_star_arr[i]
-            self.Wil[i,0] = self.kappa/2*np.power(self.bins_[0] - ri,2)
+            self.Wil[i,0] = self.beta*self.kappa/2*np.power(self.bins_[0] - ri,2)
 
             for l in range(1,self.M):
                 middle = (self.bins_[l] + self.bins_[l-1])/2
-                self.Wil[i,l] = self.kappa/2*np.power(middle-ri,2)
+                self.Wil[i,l] = self.beta*self.kappa/2*np.power(middle-ri,2)
 
 
-        self.Cil = np.exp(-self.Wil*self.beta)
+        self.Cil = np.exp(-self.Wil)
 
-        self.gi0 = np.zeros(self.S,) - 1e-8
+        self.gi0 = np.zeros(self.S,) + 1e-8
         
 
     def optimize_fn(self, gi, Ni, Ml, Cil):
         
         first_term = - (Ni*gi).sum()
-        log_pl = nup.log(Ml) - nup.log(nup.array([Ni[i]*Cil[i,:]*nup.exp(gi[i]) for i in range(self.S)]).sum(axis = 0))
-        second_term = - (Ml*log_pl).sum()
+        log_pl = nup.log(Ml) - alogsumexp(nup.repeat(gi[:,nup.newaxis],self.M,axis=1)-self.Wil, axis=0, b=nup.repeat(Ni[:,nup.newaxis],self.M,axis=1))
+        second_term = - (Ml*log_pl).sum(axis = 0)
+        
 
         return first_term + second_term
     
     def MLE_optimize(self):
         
+
         result = minimize(value_and_grad(self.optimize_fn),self.gi0,args=(self.Ni,self.Ml,self.Cil),jac=True,method='L-BFGS-B')
 
         if result.success:
             gi = result.x
-            log_pl = np.log(self.Ml) - np.log(np.array([self.Ni[i]*self.Cil[i,:]*nup.exp(gi[i]) for i in range(self.S)]).sum(axis = 0))
+            log_pl = nup.log(self.Ml) - alogsumexp(nup.repeat(gi[:,nup.newaxis],self.M,axis=1)-self.Wil, axis=0, b=nup.repeat(self.Ni[:,nup.newaxis],self.M,axis=1))
             F = -log_pl
             F = F - np.min(F)
             
+            figure(figsize=(8,6), dpi = 200)
             plt.plot(self.bins_,F)
+            plt.xlabel(r'$q$', fontsize = 15)
+            plt.ylabel(r'$\beta F(q)$', fontsize=15)
             
             return gi, log_pl, F
         
         else:
             print('fail to do minimization')
             
-            
-     def driving_force(self):
+    def driving_force(self):
         _,_,F = self.MLE_optimize()
         
         df = np.zeros(len(F)-1,)
@@ -117,5 +123,32 @@ class binwham:
         plt.ylabel(r'$\beta \frac{\partial F}{\partial q}$', fontsize=15)
         
         return self.bins_[1:], df
-        
-        
+    
+def alogsumexp(a, axis=None, b=None):
+    
+    '''
+    Calculates logsumexp using the autograd numpy
+    np.log(np.sum(b*np.exp(a)))
+    '''
+    
+    if b is not None:
+        if nup.any(b ==0):
+            a = a + 0.
+            a[b == 0] = -nup.inf
+    
+    max_a = nup.amax(a,axis=axis,keepdims=True)
+
+    if b is not None:
+        b = nup.asarray(b)
+        tmp = b*nup.exp(a - max_a)
+    else:
+        tmp = nup.exp(a-max_a)
+
+    # Suppress warning about log zero
+    with nup.errstate(divide='ignore'):
+        s = nup.sum(tmp, axis=axis)
+    
+    out = nup.log(s)
+    max_a = nup.squeeze(max_a, axis = axis)
+
+    return max_a + out
